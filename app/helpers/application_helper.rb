9a6f1ea0e92cfb6ac98e790d2b699e7af50581bc
thunderenlight <thunderenlight@gmail.com>
2016-04-05 14:40:48 -0500

    Adding bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	# Returns the full title on each page.
+
+	def full_title(page_title ='')
+		base_title = "Sampled App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
