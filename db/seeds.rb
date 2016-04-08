a29292e7f03eab432c6277758544ea04c354b5b2
thunderenlight <thunderenlight@gmail.com>
2016-04-08 12:19:30 -0500

    Finishing user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@
 User.create!(name:  "Example User",
              email: "bernie@sanders.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
